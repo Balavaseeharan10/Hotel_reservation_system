412698dc9a909b28c4053c9d4f2d5ad16f55d5f9
Balavaseeharan10 <vasee887@gmail.com>
2026-05-19 10:56:46 +0530

    ui updated

--- HotelReservationSystem/src/com/client/UserInterface.java
+++ HotelReservationSystem/src/com/client/UserInterface.java
@@ -4,8 +4,8 @@ public class UserInterface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hloo");
-		
+		System.out.println("hlooo");
+	
 
 	}
 

b7584ce142ea9d5d870121481ccbc691d68f0cc0
Balavaseeharan10 <vasee887@gmail.com>
2026-05-19 18:09:51 +0530

    updated

--- HotelReservationSystem/src/com/management/RIResidentManagement.java
+++ HotelReservationSystem/src/com/management/RIResidentManagement.java
@@ -1,5 +1,32 @@
 package com.management;
 import java.util.*;
+
+import com.model.RIResident;
 public class RIResidentManagement {
+
+	public boolean insertRIResidentList(List<RIResident> residentList) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateRIResidentPhoneNumberUsingResidentId(String residentId, long newPhoneNumber) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateOccupancyUsingResidentId(String residentId, int adults, int above12, int above5) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public RIResident retrieveRIResidentList(String residentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean deleteRIResidentDetailsFromDB(String residentId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
